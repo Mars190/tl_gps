1def1bda2a13d3561f14e9532dc6ab025e50c9c4
Mars190 <105299503+Mars190@users.noreply.github.com>
2022-12-03 10:51:05 +0100

    smaller code, anti console

--- tl_gps/server/server.lua
+++ tl_gps/server/server.lua
@@ -1,21 +1,24 @@
 local ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj  end)
 
-local postLeitZahl = LoadResourceFile(GetCurrentResourceName(), "postals.json")
-local plz = json.decode(postLeitZahl)
+local plz = json.decode(LoadResourceFile(GetCurrentResourceName(), "postals.json"))
 
 RegisterCommand("gps", function(source, args, rawCommands)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if args[1] == nil then return xPlayer.showNotification("Keine Postleitzahl angegeben") end
-    if tonumber(args[1]) == nil then return xPlayer.showNotification("Keine Postleitzahl angegeben") end
-    local coords
-    for k, v in pairs(plz) do
-        if v.code == args[1] then 
-            coords = true
-            TriggerClientEvent("tl_gps:setWaypoint", source, v.x, v.y)
-            xPlayer.showNotification("Wegpunkt gesetzt")
-            break
+    if source ~= 0 then 
+        local xPlayer = ESX.GetPlayerFromId(source)
+        if args[1] == nil then return xPlayer.showNotification("Keine Postleitzahl angegeben") end
+        if tonumber(args[1]) == nil then return xPlayer.showNotification("Keine Postleitzahl angegeben") end
+        local coords
+        for k, v in pairs(plz) do
+            if v.code == args[1] then 
+                coords = true
+                TriggerClientEvent("tl_gps:setWaypoint", source, v.x, v.y)
+                xPlayer.showNotification("Wegpunkt gesetzt")
+                break
+            end
         end
+        if coords == nil then return xPlayer.showNotification("Postleitzahl nicht gefunden") end
+    else
+        print("Du kannst diesen Command nicht von der Console ausführen")
     end
-    if coords == nil then return xPlayer.showNotification("Postleitzahl nicht gefunden") end
 end)
